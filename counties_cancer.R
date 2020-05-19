9dbd52999709f2c562b9662558542c256868445d
dchiang00 <dchiang123@hotmail.com>
2020-05-18 21:52:20 -0700

    complete four visualizations

--- counties_cancer.R
+++ counties_cancer.R
@@ -1,9 +1,8 @@
 library(ggplot2)
 library(dplyr)
-library(tidyr)
 
 # Read in data
-wa_cancer <- read.csv("data/cancer_WA.csv") 
+wa_cancer <- read.csv("../data/cancer_WA.csv") 
 
 # Finding counties with the 10 highest mortality rates in WA
 grouped_data <- wa_cancer %>%
@@ -18,5 +17,6 @@ top_10_counties <- ggplot(grouped_data, fill = location_name) +
   geom_col(aes(x = location_name, y = Mortality_Rate, fill = location_name), 
            position = 'dodge') + 
   labs(title = "Counties with the Highest Mean Mortality Rates in WA", x = "County",
-       y = "Mortality Rate", fill = "County Name")
+       y = "Mortality Rate", fill = "County Name") +
+  coord_flip()
   
--- prep_data.R
+++ prep_data.R
@@ -1,16 +1,16 @@
 library(dplyr)
 
-# read in datasets
-data_bc <- read.csv("./data_ethnicity/BreatCancerRace.csv")
-data_rc <- read.csv("./data_ethnicity/ColonandRectCancerRace.csv")
-data_lc <- read.csv("./data_ethnicity/lungCancerRace.csv")
+# Read in datasets
+data_bc <- read.csv("data/data_ethnicity/BreatCancerRace.csv")
+data_rc <- read.csv("data/data_ethnicity/ColonandRectCancerRace.csv")
+data_lc <- read.csv("data/data_ethnicity/lungCancerRace.csv")
 
-# check for data type
+# Check for data type
 sapply(data_bc, class)
 sapply(data_rc, class)
 sapply(data_lc, class)
 
-#filter only columns we need 
+# Filter only columns we need 
 data_bc <- data_bc %>%
   select(-c("Area","Year","Sex","Population","CaseCount"))
 
@@ -20,23 +20,9 @@ data_rc <- data_rc %>%
 data_lc <- data_lc %>%
   select(-c("Area","Year","Sex","Population","CaseCount"))
 
-
-# merge data sets 
+# Merge data sets 
 combine1 <- merge(data_bc,data_rc,all=TRUE)
 combine2 <- merge(combine1,data_lc,all=TRUE)
 
-
-# write new dataframe to csv file
+# Write new dataframe to csv file
 write.csv(combine2, "cancer_by_race.csv")
-
-
-  
-
-
-
-
-
-
-
-
-
