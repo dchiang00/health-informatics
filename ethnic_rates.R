f1ee1b97182656fcbe686babd83303c50759852f
dchiang00 <dchiang123@hotmail.com>
2020-05-18 21:14:48 -0700

    fix tooltip

--- ethnic_rates.R
+++ ethnic_rates.R
@@ -3,7 +3,10 @@ library(ggplot2)
 # Read in data
 cr <- read.csv("../data/cancer_by_race.csv")
 
-cr_plot <- ggplot(cr, aes(factor(CancerType), AgeAdjustedRate, fill = Race)) + 
+cr_plot <- ggplot(cr, aes(factor(CancerType), AgeAdjustedRate, fill = Race,
+                          text = paste("Cancer: ", CancerType,
+                                       "<br>Age Adjusted Rate: ", AgeAdjustedRate,
+                                       "<br>Race: ", Race))) + 
   geom_bar(stat="identity", position = "dodge") +
   ggtitle("Cancer Types by Ethnicity") +
   xlab("Cancer Type") +
